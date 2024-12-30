31f12b4438cc3fffbe940da71d82648f0100960b
Islam <islammsalam2002@gmail.com>
2024-12-30 17:29:05 +0200

    add currency & price classes

--- PublisherDomain/Product.cs
+++ PublisherDomain/Product.cs
@@ -1,4 +1,19 @@
 ﻿namespace PublisherDomain
 {
-   
+    public class Price
+    {
+        public double ItemPrice { get; set; }
+        public Currency Curr { get; set; }
+
+        public Price(double itemPrice, Currency curr)
+        {
+            ItemPrice = itemPrice;
+            Curr = curr;
+        }
+
+        public override string ToString()
+        {
+            return $"{ItemPrice} {Curr}";
+        }
+    }
 }
