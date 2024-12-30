2056c27dcf7f0d7b365511429795b5ff1569d936
Islam <islammsalam2002@gmail.com>
2024-12-30 17:33:58 +0200

    edit dbcontext file

--- PublisherData/InventoryDBContext.cs
+++ PublisherData/InventoryDBContext.cs
@@ -6,13 +6,18 @@ namespace PublisherData
 
     public class InventoryDBContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            
             optionsBuilder.UseSqlServer("Data Source=DESKTOP-SD5AB36\\SQLEXPRESS; " +
                                         "Database=Inventory2; User Id=islam; Password=Must@2002Islam;" +
                                         " Encrypt=False;");
         }
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().OwnsOne(p => p.Price);
+        }
     }
 }
